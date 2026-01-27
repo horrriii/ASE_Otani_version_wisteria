e2edebdaaa82283603d3eac1f7125bfc72a335aa
horrriii <u765325j@ecs.osaka-u.ac.jp>
2026-01-27 13:50:49 +0900

    improve wisteria module

--- ase/wisteria/modules.py
+++ ase/wisteria/modules.py
@@ -5,17 +5,17 @@ from typing import Optional
 
 
 def make_calculation_directory(
-    directory_name: str, outdir: str, srcdir_of_tmp: Optional[str] = None
+    directory_name: str, outdir: str, srcdir_of_tmp: Optional[Path] = None
 ):
-    DFT_path = os.getcwd() + "/" + directory_name
-    target_path = DFT_path + "/" + outdir
-    link_path = target_path.replace("work/03", "data/scratch", 1)
-    kernel_path = str(Path("~/QE/src/vdW_kernel_table").expanduser())
+    DFT_path = Path.cwd() / directory_name
+    target_path = DFT_path / outdir
+    link_path = Path(str(target_path).replace("work/03", "data/scratch", 1))
+    kernel_path = Path("~/QE/src/vdW_kernel_table").expanduser()
 
-    os.makedirs(name=directory_name, exist_ok=True)
+    os.makedirs(name=DFT_path, exist_ok=True)
     if srcdir_of_tmp is not None:
-        srcdir_of_tmp = str((Path(srcdir_of_tmp) / "tmp").expanduser())
-        srcdir_of_tmp = srcdir_of_tmp.replace("work/03", "data/scratch", 1)
+        srcdir_of_tmp = (srcdir_of_tmp / "tmp").expanduser()
+        srcdir_of_tmp = Path(str(srcdir_of_tmp).replace("work/03", "data/scratch", 1))
         if os.path.lexists(link_path):
             if os.path.islink(link_path):
                 os.unlink(link_path)
@@ -28,37 +28,38 @@ def make_calculation_directory(
         os.makedirs(name=link_path, exist_ok=True)
     if os.path.lexists(target_path):
         os.remove(target_path)
-    if os.path.lexists(os.path.join(DFT_path, "vdW_kernel_table")):
-        os.remove(os.path.join(DFT_path, "vdW_kernel_table"))
+    if os.path.lexists(DFT_path / "vdW_kernel_table"):
+        os.remove(DFT_path / "vdW_kernel_table")
     os.symlink(link_path, target_path)
-    os.symlink(kernel_path, os.path.join(DFT_path, "vdW_kernel_table"))
+    os.symlink(kernel_path, DFT_path / "vdW_kernel_table")
+
 
 def copy_temp_file(directory_name: str, outdir: str, save_dir: str, prefix: str):
-    DFT_path = os.getcwd() + "/" + directory_name
-    target_path = DFT_path + "/" + outdir
+    DFT_path = Path.cwd() / directory_name
+    target_path = DFT_path / outdir
 
     os.makedirs(name=save_dir, exist_ok=True)
     for i in [".1drism", ".esm1", ".rism1"]:
-        src_path = target_path + "/" + prefix + i
+        src_path = target_path / f"{prefix}{i}"
         if Path(src_path).exists():
             shutil.copy(src=src_path, dst=save_dir)
         else:
             pass
 
-def get_total_charge(src_path:Path):
-    with open(src_path / 'espresso.pwo') as f:
-        lines = [line for line in f if 'FCP: Total Charge' in line]
-    
+
+def get_total_charge(src_path: Path):
+    with open(src_path / "espresso.pwo") as f:
+        lines = [line for line in f if "FCP: Total Charge" in line]
+
     if not lines:
-        with open(src_path / 'espresso.pwi') as f:
-            lines = [line for line in f if 'tot_charge' in line]
+        with open(src_path / "espresso.pwi") as f:
+            lines = [line for line in f if "tot_charge" in line]
         final_line = lines[-1]
-    
+
     else:
         final_line = lines[-1]
-    
-    final_charge = final_line.split('=')[1]
-    final_charge = float(final_charge)
-    
-    return final_charge
 
+    final_charge = final_line.split("=")[1]
+    final_charge = float(final_charge)
+
+    return final_charge
